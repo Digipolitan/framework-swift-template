d94103f826715fb832721d8c6c7595e842fc700b
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2016-11-30 10:55:00 +0100

    first framework commit

---
+++ Podfile
@@ -0,0 +1,19 @@
+target 'DGFrameworkTemplate-iOS' do
+  use_frameworks!
+  platform :ios, '8.0'
+end
+
+target 'DGFrameworkTemplate-OSX' do
+  use_frameworks!
+  platform :osx, '10.9'
+end
+
+target 'DGFrameworkTemplate-tvOS' do
+  use_frameworks!
+  platform :tvos, '9.0'
+end
+
+target 'DGFrameworkTemplate-watchOS' do
+  use_frameworks!
+  platform :watchos, '2.0'
+end
---
+++ Sources/TemplateClass.swift
@@ -0,0 +1,14 @@
+/**
+ * Sample class for the DGFramework Template, you must delete this file
+ * @author Benoit BRIATTE http://www.digipolitan.com
+ * @copyright 2016 Digipolitan. All rights reserved.
+ */
+open class TemplateClass {
+
+}
+
+extension TemplateClass: CustomStringConvertible {
+    open var description: String {
+        return "TemplateDescription"
+    }
+}
