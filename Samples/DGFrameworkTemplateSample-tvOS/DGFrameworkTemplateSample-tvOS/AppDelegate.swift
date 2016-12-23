c4a76c27efe99235e5f8e7c2e95170d5bc28023b
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2016-12-23 17:04:13 +0100

    move OS X  and rename tvOS

--- Samples/DGFrameworkTemplateSample-tvOS/DGFrameworkTemplateSample-tvOS/AppDelegate.swift
+++ Samples/DGFrameworkTemplateSample-tvOS/DGFrameworkTemplateSample-tvOS/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  DGFrameworkTempalteSample-tvOS
+//  DGFrameworkTemplateSample-tvOS
 //
 //  Created by Benoit BRIATTE on 23/12/2016.
 //  Copyright © 2016 Digipolitan. All rights reserved.
